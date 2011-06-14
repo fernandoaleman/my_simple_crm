cb3cbace90c40e08f10dbf80c359f6bf901d79db
Fernando Aleman <fernandoaleman@mac.com>
2011-06-14 18:39:47 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySimpleCrm::Application.config.secret_token = '92bbfbb0aff40c01008fba88872f04c2db5ee58e6b850ad8755512e38cc15a0387bb1d211e9b758ee397a9fbed7fb8c1100bdc2a8e5ac62bc7be6cf77ba9c1d5'
